111557cab8f347aac0dd92dfd942680f28bd52f0
Kuati Septiani <kuatiseptiani@gmail.com>
2019-12-30 01:37:23 +0700

    first routing

---
+++ app.js
@@ -0,0 +1,18 @@
+//app.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// import routes for the products
+const product = require('./routes/product.route');
+
+//initialize our express app
+const app = express();
+
+app.use('/products', product);
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
